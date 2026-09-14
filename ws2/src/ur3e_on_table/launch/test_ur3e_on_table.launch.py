d189713bf01c8b3792252527870d26a268c9628e
Ryan Wurtz <rwurtz@purdue.edu>
2026-09-14 04:33:25 +0000

    step 4,3

---
+++ ws2/src/ur3e_on_table/launch/test_ur3e_on_table.launch.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("ur3e_on_table"),
+                    "urdf",
+                    "ur3e_on_table.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config = PathJoinSubstitution(
+        [
+            FindPackageShare("ur3e_on_table"),
+            "rviz",
+            "ur3e_on_table_default.rviz",
+        ]
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="ur3e_on_table",
+                executable="test",
+                output="screen",
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="both",
+                parameters=[robot_description],
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="both",
+                arguments=["-d", rviz_config],
+            ),
+        ]
+    )
--- ws2/src/ur3e_on_table/setup.py
+++ ws2/src/ur3e_on_table/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
@@ -29,6 +29,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'test = ur3e_on_table.joint_publisher_test:main',
+            'lissajous = ur3e_on_table.joint_publisher_lissajous:main',
         ],
     },
 )
